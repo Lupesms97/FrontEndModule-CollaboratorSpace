0f662e5592345d563e8f2c9d2ad2046fae8d12a6
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-04-11 17:14:56 -0300

    dev: adding the component to this page

--- src/app/pages/trails/video/video.component.ts
+++ src/app/pages/trails/video/video.component.ts
@@ -1,28 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
-  styleUrls: ['./video.component.css']
+  styleUrls: ['./video.component.css'],
+
 })
-export class VideoComponent {
+export class VideoComponent {}
 
-  @Output() videoFinished = new EventEmitter<void>();
-
-  
-  videoId = 'k4zh9MP5LhA';
-
-  constructor() { }
-
-  ngOnInit() {
-    const tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/iframe_api';
-    document.body.appendChild(tag);
-  }
-
-  onStateChange(event: any) {
-    if (event.data === YT.PlayerState.ENDED) {
-      this.videoFinished.emit();
-    }
-  }
-}
